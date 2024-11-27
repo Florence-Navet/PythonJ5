64658f00bb9f8ed4025235b1f3d6854cd9040dd5
AeRiaSaito <florence.navet@laplateforme.io>
2024-11-27 16:32:12 +0100

    job2

---
+++ main1.py
@@ -0,0 +1,8 @@
+#fonction retounant une liste
+def get_fruits():
+    fruits = ["pomme", "cerise", "orange"]
+    return fruits
+
+#apppel de la fonction et affichage du résultat
+resultat = get_fruits()
+print(resultat)
---
+++ main2.py
@@ -0,0 +1,8 @@
+#fonction retounant une liste
+def affiche_deuxieme_fruit():
+    fruits = ["pomme", "cerise", "orange"]
+    print(f"Affichez le deuxième fruit est: {fruits[1]}")
+
+#Appel de la fonction
+affiche_deuxieme_fruit()
+    
