d7511ead39faeb5901a48793d99895d2a60f18ef
enolaworld <enolaworld@gmail.com>
2024-11-30 14:48:47 +0100

    correction job14

--- main14.py
+++ main14.py
@@ -1,26 +1,26 @@
 def my_long_word(min_length, text):
-        #initialiser une liste pour stocker les mots qui respectent la conditions
-        long_word = []
-        #decouper la chaine de texte en mots(par espace)
-        word = ""
+    # Initialiser une liste pour stocker les mots qui respectent la condition
+    long_word = []
+    
+    # Découper la chaîne de texte en mots (par espace)
+    word = ""
 
-        for char in text:
-                if char == " ":
-                        if len(word) > min_length:
-                                long_word.append(word)
-                        word =""#reinitialiser le mot pour le prochain
-                else:
-                word += char
-        
-        
-        if len(word) > min_length: 
+    for char in text:
+        if char == " ":
+            if len(word) > min_length:
                 long_word.append(word)
+            word = ""  # Réinitialiser le mot pour le prochain
+        else:
+            word += char  # Ajouter le caractère au mot en cours
 
-        return " ".join(long_word)
+    # Ajouter le dernier mot si nécessaire
+    if len(word) > min_length: 
+        long_word.append(word)
 
-text = "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mère à la haine, la haine mère à la souffrance"
+    # Retourner la liste des mots plus longs que la longueur spécifiée
+    return " ".join(long_word)
+
+# Exemple d'utilisation
+text = "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance"
 result = my_long_word(3, text)
 print(result)
-
-   
-       
